7dbe52106d95609f8e70c950e8f692e64d02c844
Gourab <gourab.biswas@robendevs.com>
2026-01-09 08:52:59 +0600

    scope and mass assignment

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+
+    //allow mass assignment
+    // protected $fillable=['name','email','status'];
+
+    //Guarded by mass assignment
+    protected $guarded = [];
+
+    public function scopeActive($query){
+      return $query->where('status', 1);
+    }
+
+    public function scopeInactive($query){
+      return $query->where('status',0);
+    }
 }
--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -11,8 +11,8 @@ class CustomersController extends Controller
     public function list()
     {
 
-        $activeCustomers = Customer::where('status', 1)->get();
-        $inactiveCustomers = Customer::where('status',0)->get();
+        $activeCustomers = Customer::active()->get();
+        $inactiveCustomers = Customer::inactive()->get();
 
         // return view('internals/customers', [
         //     'activeCustomers' => $activeCustomers,
@@ -30,14 +30,7 @@ class CustomersController extends Controller
             'status' => 'required'
         ]);
 
-
-
-        $customer = new Customer();
-        $customer->name = request('name');
-        $customer->email = request('email');
-        $customer->status = request('status');
-        $customer->save();
-
+        Customer::create($data);
 
         return back();
     }
